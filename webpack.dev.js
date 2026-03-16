068616d554b86ff689ff7f7fd72a3a8823a94509
Austine Juma <austinejumah001@gmail.com>
2026-03-16 15:38:55 +0300

    chore: project setup with webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,20 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js"
 
-
 export default merge(common, {
     mode: "development",
+       module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
     devtool: "eval-source-map",
     devServer: {
         static: "./dist",
         watchFiles: ["src/template.html"],
-    }
+    },
+ 
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,25 @@
 import {merge} from "webpack-merge";
 import common from "./webpack.common.js";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default merge(common, {
     mode: "production",
-    devtool: "source-map"
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            }
+        ]
+    },
+     plugins: [
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+        }),
+    ],
+    optimization: {
+        minimizer: ["...", new CssMinimizerPlugin()]
+    }
 })
